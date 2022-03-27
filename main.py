cf1d397bffdca92fc84945f32444d4f090eb0d5c
Harshit <quantumbytestudios@gmail.com>
2022-03-28 00:13:12 +0530

    Main File

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# THIS IS RAW CODE NOT TO RUN!!
+
+# sudo apt install tesseract
+
+import os
+
+os.system("clear")
+
+ImageFilePath = input(
+    "Image File Ka Path De Without Extension (PNG he chaiye merko): ")
+
+OutputFile = ImageFilePath+".png"
+
+os.system(f"tesseract {OutputFile} {ImageFilePath}")
+
+os.system("clear")
+print("File Contents are: \n")
+os.system(f"cat {ImageFilePath}.txt")
